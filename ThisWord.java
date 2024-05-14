92c754f1cd3ddc3b4f0eea0c3522bc033ce672ff
Ruslan <ruslanmarlinskiy@gmail.com>
2024-05-14 06:46:12 +0300

    Add files via upload

---
+++ ThisWord.java
@@ -0,0 +1,26 @@
+public class ThisWord {
+    public static void main(String[] args) {
+        Realisation pups = new Realisation();
+        pups.setName("Rusya");
+        pups.setAge(24);
+        pups.bark();
+    }
+}
+
+
+class Realisation {
+    String name;
+    int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void bark() {
+        System.out.println(name + " " + age);
+    }
+}
